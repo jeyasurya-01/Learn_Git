ceb960253bf87c9e1e9d87438fa67fb9f0ee042d
jeyasurya-01 <127177549+jeyasurya-01@users.noreply.github.com>
2023-03-07 12:31:11 +0530

    Update SampleCode.java

--- Learn_git/src/main/java/SampleCode.java
+++ Learn_git/src/main/java/SampleCode.java
@@ -3,7 +3,7 @@ public class SampleCode {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hosted this file in git");
+		System.out.println("new commit added with new changes in main branch");
 
 	}
 
