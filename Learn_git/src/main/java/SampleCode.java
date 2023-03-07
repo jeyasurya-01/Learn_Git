2255a10c7394ad4a101eba47d6cab7eb27a5d221
jeyasurya-01 <jeyasuryajsu2301@gmail.com>
2023-03-07 12:46:04 +0530

    new code added in test branch samplecode.java

--- Learn_git/src/main/java/SampleCode.java
+++ Learn_git/src/main/java/SampleCode.java
@@ -2,8 +2,8 @@
 public class SampleCode {
 
 	public static void main(String[] args) {
-		
-		System.out.println("changes made in new test_branch");
+
+		System.out.println("prev code deleted fresh code added");
 
 	}
 
